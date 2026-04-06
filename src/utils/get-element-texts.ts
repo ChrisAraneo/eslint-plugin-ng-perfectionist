fbd4d4abe0dbba8aa81b8d57a5ab73cbe0bf9e15
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-06 14:19:13 +0200

    feat: getElementTexts util function

---
+++ src/utils/get-element-texts.ts
@@ -0,0 +1,8 @@
+import { type TSESTree } from '@typescript-eslint/utils';
+
+import type { RuleContext } from '@typescript-eslint/utils/ts-eslint';
+
+export const getElementTexts = (
+  elements: ((TSESTree.Expression | TSESTree.SpreadElement))[],
+  sourceCode: RuleContext<string, []>['sourceCode'],
+): string[] => elements.map((el) => sourceCode.getText(el));
